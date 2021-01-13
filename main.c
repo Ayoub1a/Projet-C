26f6676329578d31173676396fcdfd28cc2a6a04
Ayoub Bouallal <71018302+Ayoub-2@users.noreply.github.com>
2021-01-13 16:16:36 +0100

    Menu version 2 : position des options

--- main.c
+++ main.c
@@ -3,13 +3,16 @@
 #include <strings.h>
 void clear(); 
 int Menu();
+void Menu_Adherant();
+void Menu_Livres();
+void Menu_Emprunt();
 int main() {
     Menu();
 	return 0 ;
 }
 int Menu() {
-	char c = "g"; 
-	while(c!= 113 ) {
+	char c; 
+	do {
 		clear() ;
 		printf("Bonjour !!\n");
 		printf("Choisissez nentre : \n");
@@ -20,18 +23,16 @@ int Menu() {
 		//printf("%c" , c);
 		switch(c) {
 			case 49:
-				printf("adherant\n");
+				Menu_Adherant();
 				break;
 			case 50: 
-				printf("livres\n");
+				Menu_Livres();
 				break;
 			case 51:
-				printf("emprunt\n");
+				Menu_Emprunt();
 				break;
-			default : 
-				printf("rien\n");
 		}
-	}
+	}while(c!=113);
 	return 0 ;
 }
 void clear(){
@@ -43,3 +44,112 @@ void clear(){
         system("cls");
     #endif
 }
+void Menu_Adherant() {
+	/*
+void Remplir_Adherent(char fichier[]); //
+list_Adherents Charger_Adherents(char fichier[]); //
+void afficherAdherent(Adherent * A); //
+void Ajouter_Adherent(list_Adherents* list_adh); //
+Adherent* Rechercher_Adherent_num(list_Adherents list_adh,int numero); //
+Adherent* Rechercher_Adherent_nom(list_Adherents list_adh,char nom[]);//
+void Modifier_Adherent(list_Adherents * list_adh,int numero); //
+void Sauvegarder_Adherents(char fichier[], list_Adherents list_adh); //
+void Supprimer_Adherent(list_Adherents *list_adh, int numero); //
+	*/
+	char c; 
+	do {
+		clear() ;
+		printf("Bonjour !!\n");
+		printf("Choisissez entre : \n");
+		printf("1. Remplir adherant.\n");
+		printf("2. Charger adherant.\n");
+		printf("4. Afficher Adherant.\n");
+		printf("5. Ajouter Adherant.\n");
+		printf("6. Rechercher Adherant.\n");
+		printf("7. Modifier Adherant.\n");
+		printf("8. Sauvegarder Adherant.\n");
+		printf("9. Supprimer Adherant.\n");
+		c = getchar();
+		//printf("%c" , c);
+		switch(c) {
+			case 49:
+				
+				break;
+			case 50:
+				break;
+			case 51:
+				break;
+		}
+	}while(c!=113);
+}
+void Menu_Livres() {
+	/*
+void Remplir_Livres(char fichier[]);//done
+list_livres Charger_Livres(char fichier[]); //done
+void Ajouter_Livre(list_livres* list_lv); //done
+Livre* Rechercher_Livre_num(list_livres list_lv,int numero);//done
+void Modifier_Livre(list_livres* list_lv,int numero);//done
+void Supprimer_Livre(list_livres* list_lv,int numero);//done
+Livre Rechercher_Livre_cat(list_livres list_lv,char categorie[],char titre[]);
+void Ordonner_Categorie(list_livres* list_lv);//done
+void Sauvegarder_Livres(char fichier[] , list_livres);//done
+void afficherListeLivres(Livre* L);//done
+	*/
+	char c; 
+	do {
+		clear() ;
+		printf("Bonjour !!\n");
+		printf("Choisissez entre : \n");
+		printf("1. Remplir livre.\n");
+		printf("2. Charger livre.\n");
+		printf("3. Afficher livre.\n");
+		printf("4. Ajouter livre.\n");
+		printf("5. Rechercher livre.\n");
+		printf("6. Modifier livre.\n");
+		printf("7. Sauvegarder livre.\n");
+		printf("8. Supprimer livre.\n");
+		c = getchar();
+		//printf("%c" , c);
+		switch(c) {
+			case 49:
+				
+				break;
+			case 50:
+				break;
+			case 51:
+				break;
+		}
+	}while(c!=113);
+}
+void Menu_emprunt() {
+	/*
+void Emprunter(int numero_Adherent , int numero_livre ,list_livres * list_lv , list_Adherents *list_adh ); //
+// donner le numero d'Adherent  , rechercher par nom le livre
+void Afficher_livres_Empruntes(list_livres list_lv); //
+void Afficher_Adherents_emprunteurs(list_livres list_lv, list_Adherents list_adh);//
+// eliminer la redendance des emprunteurs
+void Rendre_livre(list_livres *list_lv ,list_Adherents* list_adh ,int numero_lv);//
+	*/
+	char c; 
+	do {
+		clear() ;
+		printf("Bonjour !!\n");
+		printf("Choisissez entre : \n");
+		printf("1. Emprunter.\n");
+		printf("2. Afficher livre emprunte.\n");
+		printf("3. Afficher adherants emprunteur.\n");
+		printf("4. Rendre livre.\n");
+		c = getchar();
+		//printf("%c" , c);
+		switch(c) {
+			case 49:
+				
+				break;
+			case 50:
+				break;
+			case 51:
+				break;
+		}
+	}while(c!=113);
+}
+//color 
